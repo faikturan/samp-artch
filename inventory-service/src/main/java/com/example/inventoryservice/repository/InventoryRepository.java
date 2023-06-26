0840566950531f7ba403870e483dde5b1fa8a575
Faik Turan <turanfaik@hotmail.com>
2023-06-26 12:10:02 +0300

    first-step

---
+++ inventory-service/src/main/java/com/example/inventoryservice/repository/InventoryRepository.java
@@ -0,0 +1,13 @@
+package com.example.inventoryservice.repository;
+
+import com.example.inventoryservice.model.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+
+    default Optional<Inventory> findBySkuCode() {
+        return null;
+    }
+}
---
+++ order-service/src/main/java/com/example/orderservice/service/OrderService.java
@@ -0,0 +1,58 @@
+package com.example.orderservice.service;
+
+import com.example.orderservice.dto.OrderLineItemDto;
+import com.example.orderservice.dto.OrderRequest;
+import com.example.orderservice.model.Order;
+import com.example.orderservice.model.OrderLineItems;
+import com.example.orderservice.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+    private final WebClient webClient;
+
+    public void placeOrder(OrderRequest orderRequest){
+        Order order = new Order();
+        order.setOrderNumber(UUID.randomUUID().toString());
+
+        List<OrderLineItems> orderLineItems = orderRequest.getOrderLineItemDtoList()
+                .stream()
+                .map(this::mapToDto)
+                .toList();
+
+        order.setOrderLineItemsList(orderLineItems);
+
+        //Call Inventory Service, and place order if product is in stock
+        Boolean result = webClient.get()
+                .uri("http://localhost:8082/api/inventory")
+                .retrieve()
+                .bodyToMono(Boolean.class)
+                .block();
+
+        if (result){
+            orderRepository.save(order);
+        }else{
+            throw new IllegalArgumentException("Product is not in stock, please try again later");
+        }
+
+    }
+
+    private OrderLineItems mapToDto(OrderLineItemDto orderLineItemDto) {
+        OrderLineItems orderLineItems =  new OrderLineItems();
+        orderLineItems.setPrice(orderLineItemDto.getPrice());
+        orderLineItems.setQuantity(orderLineItemDto.getQuantity());
+        orderLineItems.setSkuCode(orderLineItemDto.getSkuCode());
+        return orderLineItems;
+    }
+
+}
